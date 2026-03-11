4dabd956dd31ca73ac3eadcde1363ead4f358a1f
FahadHussain622 <fahadriaz622@gmail.com>
2026-03-11 23:48:55 +0500

    fixed amplify v6 import error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import Amplify from "aws-amplify"
+import { Amplify } from "aws-amplify"
 import awsconfig from "./aws-exports"
 
 Amplify.configure(awsconfig)
