4dd47290c74563eba9ed9f12ba370b382b4745f4
FahadHussain622 <fahadriaz622@gmail.com>
2026-03-12 01:04:46 +0500

    forced latest apollo client version

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,8 +1,10 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
+  link: new HttpLink({
   uri: "https://tjkxyrmeknhk7esba3snjb6l3q.appsync-api.us-east-1.amazonaws.com/graphql",
-  cache: new InMemoryCache()
-})
+  }),
+  cache: new InMemoryCache(),
+});
 
-export default client
+export default client;
